8dea97c3a55e5422a1d8ec98437e2b63131231e0
Nathan Lucy <cosmocoder1@gmail.com>
2021-12-13 14:30:01 -0600

    css refactor to allow for Chrome Dev tool use.  Product Info refactor to stateless

--- subcomponents/overview/ProductInfo.jsx
+++ subcomponents/overview/ProductInfo.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 
-class ProductInfo extends React.Component {
+var ProductInfo = () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
 
-    }
-  }
-
-  render() {
     return (
 
       <div className={'productInfo'}>Product Info</div>
 
     )
-  }
+
 
 };
 
