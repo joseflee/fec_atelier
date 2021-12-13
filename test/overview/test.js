f4b58968f02d4ef83a5fd24fe5cf4cee3cbd0179
Nathan Lucy <cosmocoder1@gmail.com>
2021-12-13 12:27:47 -0600

    enzyme successfully testing Overview component

--- test/overview/test.js
+++ test/overview/test.js
@@ -1,8 +1,12 @@
-import { expect } from 'chai';
 import React from 'react';
-import Overview from '../../components/Overview.jsx';
 import Enzyme from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { expect } from 'chai';
+import Overview from '../../components/Overview.jsx';
+
+
+
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -10,19 +14,21 @@ Enzyme.configure({ adapter: new Adapter() });
 
 var container;
 
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+// beforeEach(() => {
+//   container = document.createElement("div");
+//   document.body.appendChild(container);
+// });
 
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
+// afterEach(() => {
+//   document.body.removeChild(container);
+//   container = null;
+// });
 
 describe("app component test", () => {
 
-  // it("Renders successfully", () => {
-  // })
+  it("Renders successfully", () => {
+    const wrapper = shallow(<Overview/>);
+    expect(wrapper.find('div')).to.have.length(1);
+  })
 
 })
