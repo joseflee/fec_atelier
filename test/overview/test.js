21863c3a6a026891100c69b855334c7112cc99dc
Nathan Lucy <cosmocoder1@gmail.com>
2021-12-13 13:04:10 -0600

    Sinon install, lifecycle method (componentDidMount) test successful

--- test/overview/test.js
+++ test/overview/test.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import Enzyme from 'enzyme';
-import { mount, shallow } from 'enzyme';
+import { mount, shallow, render } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { expect } from 'chai';
 import Overview from '../../components/Overview.jsx';
-
+import sinon from 'sinon';
 
 
 
 Enzyme.configure({ adapter: new Adapter() });
 
-//configure({ adapter: new Adapter() });
 
 var container;
 
-// beforeEach(() => {
-//   container = document.createElement("div");
-//   document.body.appendChild(container);
-// });
-
-// afterEach(() => {
-//   document.body.removeChild(container);
-//   container = null;
-// });
-
 describe("app component test", () => {
 
-  it("Renders successfully", () => {
+  it("Overview renders successfully", () => {
     const wrapper = shallow(<Overview/>);
     expect(wrapper.find('div')).to.have.length(1);
-  })
+  });
+
+  it("Overview runs component did mount", () => {
+    const log = sinon.spy(console, 'log');
+    const wrapper = shallow(<Overview/>);
+    if (!log.calledOnceWith('component did mount')) {
+      throw new Error('component did not mount');
+    }
+  });
 
 })
