7c540bed9d617c62581d466a75217339f6b16626
Nathan Lucy <cosmocoder1@gmail.com>
2021-12-13 19:12:55 -0600

    test suite update

--- test/overview/test.js
+++ test/overview/test.js
@@ -1,28 +1,28 @@
-import { expect } from 'chai';
 import React from 'react';
-import Overview from '../../components/Overview.jsx';
 import Enzyme from 'enzyme';
+import { mount, shallow, render } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { expect } from 'chai';
+import Overview from '../../components/Overview.jsx';
+import sinon from 'sinon';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-//configure({ adapter: new Adapter() });
-
 var container;
 
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+describe("Overview", () => {
 
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
+  it("Overview renders successfully", () => {
+    const wrapper = shallow(<Overview/>);
+    expect(wrapper.find('div')).to.have.length(1);
+  });
 
-describe("app component test", () => {
-
-  // it("Renders successfully", () => {
-  // })
+  it("Overview runs component did mount", () => {
+    const log = sinon.spy(console, 'log');
+    const wrapper = shallow(<Overview/>);
+    if (!log.calledOnceWith('component did mount')) {
+      throw new Error('component did not mount');
+    }
+  });
 
 })
