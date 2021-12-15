94172c2ae8a53b5ec2d39d8223969a9cb7401816
Joe Lee <whatjoedoing@gmail.com>
2021-12-14 20:35:07 -0800

    Added two basic click handler functions to relatedItems

--- components/RelatedItems.jsx
+++ components/RelatedItems.jsx
@@ -7,12 +7,30 @@ import {OutfitCard} from '../subcomponents/relatedItems/OutfitCard.jsx';
 class RelatedItems extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+
+    }
+
+    this.handleRelatedCardClick = this.handleRelatedCardClick.bind(this);
+    this.handleRelatedStarClick = this.handleRelatedStarClick.bind(this);
+  }
+
+  //when a card on the related items list is clicked, it will navigate to that page
+  handleRelatedCardClick(e) {
+    console.log('hello')
+  }
+  //when the star on the related item card is clicked, a comparison modal will be opened up
+  handleRelatedStarClick() {
+
   }
 
   render() {
     return (
+      <>
       <div>Related Items</div>
+      <RelatedProductList clickCard={this.handleRelatedCardClick}/>
+      <OutfitList />
+      </>
     )
   }
 }
