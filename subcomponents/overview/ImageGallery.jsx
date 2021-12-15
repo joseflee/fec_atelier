ad6ca1f12dddf27d22a802e5cc57e862b219a1fc
Nathan Lucy <cosmocoder1@gmail.com>
2021-12-14 19:14:45 -0600

    Image Gallery + thumbnail gallery + zoom view basic structure complete

--- subcomponents/overview/ImageGallery.jsx
+++ subcomponents/overview/ImageGallery.jsx
@@ -14,12 +14,45 @@ class ImageGallery extends React.Component {
       zoom: false
 
     }
+
+    this.toggleZoom = this.toggleZoom.bind(this);
+    this.switchZoomDisplay = this.switchZoomDisplay.bind(this);
+
   }
 
   // ImageZoom component has CSS display set to "none" until toggled
 
   toggleZoom() {
     // handles click on image to produce zoomed image
+    var newZoom;
+
+    if (this.state.zoom === true) {
+      newZoom = false;
+    } else {
+      newZoom = true;
+    }
+
+    this.setState({
+      ...this.state,
+      zoom: newZoom
+    }, () => {
+      this.switchZoomDisplay();
+    })
+
+
+
+  }
+
+  switchZoomDisplay() {
+
+    var zoomView = $('#zoomView');
+
+    if (this.state.zoom === true) {
+      zoomView.css('display', 'inline-block');
+    } else {
+      zoomView.css('display', 'none');
+    }
+
   }
 
 
@@ -28,9 +61,9 @@ class ImageGallery extends React.Component {
     return (
 
       <div className={'imageGallery'}>
-        <div>Image Gallery</div>
+        <div onClick={this.toggleZoom}>Main Image</div>
         <ImageInsert props={this.props} />
-        <ImageZoom props={this.state}/>
+        <ImageZoom props={this.state} />
       </div>
 
     )
--- subcomponents/overview/ImageInsert.jsx
+++ subcomponents/overview/ImageInsert.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 var ImageInsert = (props) => {
 
+  var temporaryImageProps = ['image1', 'image2', 'image3', 'image4', 'image5', 'image6', 'image7'];
+
   return (
-    <div>thumbnail image scroll</div>
+    <div className={'thumbnailGallery'}>{temporaryImageProps.map((item, i) => (
+      <div key={i} className={'thumbnailItem'}>{item}</div>
+    ))}</div>
   )
 
 }
