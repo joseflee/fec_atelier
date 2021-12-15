d10084e99dc8d1d16084df8916c01b7771f83951
Nathan Lucy <cosmocoder1@gmail.com>
2021-12-14 18:15:30 -0600

    styleSelector basic layout complete.  Styling and overflow in progress

--- subcomponents/overview/Style.jsx
+++ subcomponents/overview/Style.jsx
@@ -4,8 +4,12 @@ var Style = (props) => {
 
   //props (single style name, img and checkmark / selected data are received and style circle is rendered )
 
+  var temporaryStyleProps = ['style 1', 'style 2', 'style 3', 'style 4', 'style 5', 'style 6'];
+
   return (
-    <div className={'styleSelector'}>single style</div>
+    <div className={'styleSelector'}>{temporaryStyleProps.map((item, i) => (
+      <div key={i} className={'styleItem'}>{item}</div>
+    ))}</div>
   )
 
 }
