5d933b5239a532203ea4237c04858bbfebaf81c2
Joe Lee <whatjoedoing@gmail.com>
2021-12-17 19:05:45 -0800

    Centered arrows and click handler placeholders to the arrow buttons

--- subcomponents/relatedItems/RelatedProductList.jsx
+++ subcomponents/relatedItems/RelatedProductList.jsx
@@ -13,24 +13,45 @@ var mockRelatedItems = [
   {picture: 'sample', category: 'socks', price: '$25', rating: 3.6}
 ]
 
-var rightArrowClasses = 'arrow rightArrow';
+//where to keep the related items state
+//related items will be got from the api and will be in related Items component
+//from there pass it as props to related list
+//in related list there will be a current view container. this will be located in the list component and not the parent component.
+
 
 export class RelatedProductList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentView: [],
+    }
+    this.handleLeftArrow = this.handleLeftArrow.bind(this);
+    this.handleRightArrow = this.handleRightArrow.bind(this);
   }
 
+  handleLeftArrow() {
+    console.log('left arrow clicked');
+  }
+
+  handleRightArrow() {
+    console.log('right arrow clicked');
+  }
 
   render() {
     return (
       <>
         <h3>list of related products</h3>
         <div className="carousel">
-          <div className="leftArrow"></div>
+          <div className="centerVertical">
+            <div className="leftArrow" onClick={this.handleLeftArrow}></div>
+          </div>
           {mockRelatedItems.map((item, index) => {
             return <ProductCard key={index} clickCard={this.props.clickCard} clickStar={this.props.clickStar} itemInfo={item} />
           })}
-          <div className="rightArrow"></div>
+          <div className="centerVertical">
+            <div className="rightArrow" onClick={this.handleRightArrow}></div>
+          </div>
         </div>
       </>
     )
