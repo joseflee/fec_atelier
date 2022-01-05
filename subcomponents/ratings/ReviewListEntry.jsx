54b97af369a6f59f6dad2810fd42b61ceec193b4
Julian Ortiz <julianlortiz@yahoo.com>
2022-01-04 22:16:35 -0800

    implemented moment for readable dates

--- subcomponents/ratings/ReviewListEntry.jsx
+++ subcomponents/ratings/ReviewListEntry.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
 var ReviewListEntry = (props) => {
   var review = props.review;
+  var date = moment(review.date).format('MMMM Do YYYY');
+  // console.log('date: ', date);
   // console.log('list entry reviews: ', review);
   return (
     <div>
      <div className="reviewRating">{review.rating}</div>
      <div className="reviewerName">{review.reviewer_name}</div>
-     <div className="reviewDate">{review.date}</div>
+     <div className="reviewDate">{date}</div>
      <div className="reviewSummary">{review.summary}</div>
      <div className="recommendedCheck">{review.recommended}</div>
      <div className="sellerResponse">{review.response}</div>
