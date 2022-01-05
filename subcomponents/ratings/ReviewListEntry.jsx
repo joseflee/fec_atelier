de9ae4d55a7ba11858a05563486bbddcb40d4af7
Julian Ortiz <julianlortiz@yahoo.com>
2022-01-04 23:59:47 -0800

    helpfulness now properly increments

--- subcomponents/ratings/ReviewListEntry.jsx
+++ subcomponents/ratings/ReviewListEntry.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import moment from 'moment';
 
-var ReviewListEntry = (props) => {
-  var review = props.review;
-  var date = moment(review.date).format('MMMM Do YYYY');
-  // console.log('date: ', date);
-  // console.log('list entry reviews: ', review);
-  return (
-    <div>
-     <div className="reviewRating">{review.rating}</div>
-     <div className="reviewerName">{review.reviewer_name}</div>
-     <div className="reviewDate">{date}</div>
-     <div className="reviewSummary">{review.summary}</div>
-     <div className="recommendedCheck">{review.recommended}</div>
-     <div className="sellerResponse">{review.response}</div>
-     <div className="reviewHelpfulness">{review.helpfullness}</div>
-    </div>
-  )
+class ReviewListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      helpfulness: props.review.helpfulness
+    }
+    this.handleHelpful = this.handleHelpful.bind(this);
+    // var review = props.review;
+    // var date = moment(review.date).format('MMMM Do YYYY');
+  }
+
+  handleHelpful() {
+    console.log('click');
+    // console.log('props: ', this.props)
+    // console.log(this.state);
+    // console.log(this.props.helpfulness);
+    this.setState((state) => ({ helpfulness: (state.helpfulness + 1) }), () => {
+      console.log(this.state);
+    });
+  }
+
+  render() {
+    return (
+      <div className="reviewListEntry">
+        <div className="reviewRating">{this.props.rating}</div>
+        <div className="reviewerName">{this.props.reviewer_name}</div>
+        <div className="reviewDate">{moment(this.props.date).format('MMMM Do YYYY')}</div>
+        <div className="reviewSummary">{this.props.summary}</div>
+        {this.props.recommend ? <div className="recommendedCheck">I recommend this product</div> : null}
+        {this.props.response ? <div className="sellerResponse">Response: {this.props.response}</div> : null}
+        <div className="reviewHelpfulness">Helpful?
+          <div id="helpfulYes" onClick={this.handleHelpful}>Yes</div>
+          ({this.state.helpfulness})
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ReviewListEntry;
