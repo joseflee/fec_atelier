c05f8f8f2f62d331dcee1976f82aa02af2bc34c5
Julian Ortiz <julianlortiz@yahoo.com>
2022-01-09 19:26:41 -0800

    Implemented photo modal window

--- subcomponents/ratings/ReviewPhotos.jsx
+++ subcomponents/ratings/ReviewPhotos.jsx
@@ -5,36 +5,56 @@ class ReviewPhotos extends React.Component {
     super(props);
     this.state = {
       photos: [],
-      selected: null
+      selectedURL: null,
+      modal: false
     };
     this.handleModalView = this.handleModalView.bind(this);
-    // this.unpackImages = this.unpackImages.bind(this);
+    this.unpackImages = this.unpackImages.bind(this);
+    this.handleModalView = this.handleModalView.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
   }
 
-  // componentDidMount() {
-  //   this.unpackImages();
-  // }
+  componentDidMount() {
+    this.unpackImages();
+  }
 
-  // unpackImages() {
-  //   var photos = [];
-  //   for (var i = 0; i < this.props.photos.length; i++) {
-  //     console.log('url: ', this.props.photos[i].url);
-  //     photos.push(this.props.photos[i]);
-  //   }
-  //   this.setState({photos: photos}, () => {
-  //     console.log('state: ', this.state.photos);
-  //   });
-  // }
+  unpackImages() {
+    var photos = [];
+    for (var i = 0; i < this.props.photos.length; i++) {
+      photos.push(this.props.photos[i]);
+    }
+    this.setState({photos: photos});
+  }
 
-  handleModalView() {
-    // create modal window for selected thumbnail
+  handleModalClose() {
+    this.setState({
+      modal: false
+    })
+  }
+
+  handleModalView(e) {
+    var selected = Number(e.target.id);
+    var photos = this.state.photos;
+
+    for (var i = 0; i < photos.length; i++) {
+      if (photos[i].id === selected) {
+        this.setState({
+          modal: true,
+          selectedURL: photos[i].url
+        });
+      }
+    }
+
   }
 
     render() {
       return (
         <div className="reviewPhotos">
+          {this.state.modal ? <div className="reviewPhotoWindow" onClick={this.handleModalClose}>
+            <img className="reviewModalImg" src={this.state.selectedURL}></img>
+          </div> : null}
           {this.props.photos ? this.props.photos.map((photo, index) => (
-            <img className="reviewImage" key={index} src={photo.url} onClick={this.handleModalView}></img>
+            <img className="reviewImage" key={index} src={photo.url} onClick={this.handleModalView} id={photo.id}></img>
           )) : null}
         </div>
       )
