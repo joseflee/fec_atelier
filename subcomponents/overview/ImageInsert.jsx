bcdde3163e027cbd4e8b6ffe0b5433c03916a272
Nathan Lucy <cosmocoder1@gmail.com>
2022-01-11 13:00:43 -0600

    image gallery thumbnail alignment fix

--- subcomponents/overview/ImageInsert.jsx
+++ subcomponents/overview/ImageInsert.jsx
@@ -107,10 +107,10 @@ class ImageInsert extends React.Component {
 
     if (direction === 'down' && Math.floor(offset.top) > 120) {
       shift = offset.top - 20;
-      gallery.offset({ top: shift, left: 20 });
+      gallery.offset({ top: shift });
     } else if (direction === 'up' && Math.floor(offset.top) < 266) {
       shift = offset.top + 20;
-      gallery.offset({ top: shift, left: 20 });
+      gallery.offset({ top: shift });
     }
 
 
